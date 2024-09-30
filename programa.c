a706f76a73b90e216adca1b38dd46ed643a539ad
Jose Alejandro Rojas Zapata <134547867+Jarzitop@users.noreply.github.com>
2024-09-29 20:13:01 -0500

    Codigo implementado ( creoq ue quedó bien

--- programa.c
+++ programa.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+double calcular_tasa_bruta(int datos, double tiempo) {
+    return datos / tiempo;
+}
+
+double calcular_tasa_efectiva(int datos, int datos_control, double tiempo) {
+    return (datos - datos_control) / tiempo;
+}
+
+int main() {
+    int datos, datos_control;
+    double tiempo;
+
+    printf("Ingrese la cantidad de datos transmitidos (en bits): ");
+    scanf("%d", &datos);
+
+    printf("Ingrese la cantidad de datos de control transmitidos (en bits): ");
+    scanf("%d", &datos_control);
+
+    printf("Ingrese el tiempo de transmisión (en segundos): ");
+    scanf("%lf", &tiempo);
+
+    double tasa_bruta = calcular_tasa_bruta(datos, tiempo);
+    printf("Tasa de transferencia bruta: %.2f bps\n", tasa_bruta);
+
+    double tasa_efectiva = calcular_tasa_efectiva(datos, datos_control, tiempo);
+    printf("Tasa de transferencia efectiva: %.2f bps\n", tasa_efectiva);
+
+    return 0;
+}
